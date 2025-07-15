27a40427d3e69962a07a8df2fae68b84e883852a
mestn1y <mestn1y@hotmail.com>
2025-07-15 23:45:46 +0300

    added markup and styles for meal details page,also add function getMeal

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,3 +1,30 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from "next/image";
+import css from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={css.header}>
+        <div className={css.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={css.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={css.creator}>
+            by <a href={`mailto:{meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={css.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={css.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
